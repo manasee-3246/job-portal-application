e7827415da43a268e61abaab597ec834fdfaf05c
manasee-3246 <manaseevaghasiya1@gmail.com>
2025-12-18 14:59:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import jobRoutes from "./routes/jobRoutes.js"
+import applicationRoutes  from "./routes/applicationRoutes.js";
+dotenv.config();
+connectDB();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use("/uploads",express.static("uploads"));
+app.use("/api/auth",authRoutes);
+app.use("/api/jobs",jobRoutes);
+app.use("/api/applications",applicationRoutes);
+app.listen(process.env.PORT,()=>
+    console.log(`Server running on port ${process.env.PORT}`)
+
+);
